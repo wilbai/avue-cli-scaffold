3920ffbe5f528fec01d507de10c059cf96e06671
wuwy <1058191407@qq.com>
2020-01-06 18:24:42 +0800

    feat:项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // 基础路径 注意发布之前要先修改这里
 let baseUrl = './'
 module.exports = {
-  baseUrl: baseUrl, // 根据你的实际情况更改这里
+  publicPath: baseUrl, // 根据你的实际情况更改这里
   lintOnSave: true,
   productionSourceMap: false,
   chainWebpack: (config) => {
@@ -24,16 +24,17 @@ module.exports = {
       .add('@/mock')
       .end()
   },
+  transpileDependencies: ['avue-plugin-transfer', 'avue-plugin-ueditor'],
   //配置转发代理
   devServer: {
-    // proxy: {
-    //     '/api': {
-    //         target: 'http://127.0.0.1:3000',
-    //         ws: true,
-    //         pathRewrite: {
-    //             '^/api': '/api'
-    //         }
-    //     },
-    // }
+    proxy: {
+        '/': {
+            target: 'http://127.0.0.1:3000',
+            ws: false,
+            pathRewrite: {
+                '^/api': '/api'
+            }
+        },
+    }
   }
 }
